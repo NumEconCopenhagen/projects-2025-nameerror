5de6392787cf0c648e2c739cec556117a4e2bbeb
Simon Skødeberg <hkb@ku.dk>
2026-08-12 16:10:44 +0200

    Exercise 1.4 done

--- 01_dataproject/dataproject.py
+++ 01_dataproject/dataproject.py
@@ -90,3 +90,124 @@ def load_IFOR32(DECILGEN,KOMMUNEDK,varname):
     df = df.reset_index().sort_values(by=['municipality'])
 
     return df
+
+# Create a function to load NGLK.
+def load_NGLK(OMRÅDE, BNØGLE, BRUTNETUDG, PRISENHED, varname):
+
+    params = {
+        'table': 'NGLK',
+        'format': 'BULK', # semicolon separated file
+        'lang': 'en',
+        'variables': [
+            {'code': 'OMRÅDE', 'values': [OMRÅDE]},
+            {'code': 'BNØGLE', 'values': [BNØGLE]},
+            {'code': 'BRUTNETUDG', 'values': [BRUTNETUDG]},
+            {'code': 'PRISENHED', 'values': [PRISENHED]},
+            {'code': 'Tid', 'values': ['*']},
+            ]
+    }    
+
+    # b. download
+    df = DstApi('NGLK').get_data(params=params)
+
+    # c. set types and rename
+    df['TID'] = df['TID'].astype(int)
+    df['INDHOLD'] = df['INDHOLD'].astype(float)
+    df = df.drop(columns=['BNØGLE', 'BRUTNETUDG', 'PRISENHED'])
+    df = df.rename(columns={'INDHOLD': varname, 'OMRÅDE': 'municipality', 'TID': 'year'})
+
+    # e. sorts
+    df = df.sort_values(by=['municipality'])
+    
+    return df
+
+# Create a function to load FOD407.
+def load_FOD407(OMRÅDE, ALDER, varname):
+
+    params = {
+        'table': 'FOD407',
+        'format': 'BULK', # semicolon separated file
+        'lang': 'en',
+        'variables': [
+            {'code': 'OMRÅDE', 'values': [OMRÅDE]},
+            {'code': 'ALDER', 'values': [ALDER]},
+            {'code': 'Tid', 'values': ['*']},
+            ]
+    }    
+
+    # b. download
+    df = DstApi('FOD407').get_data(params=params)
+
+    # c. set types and rename
+    df['INDHOLD'] = pd.to_numeric(df['INDHOLD'], errors='coerce')
+    df = df.dropna(subset=['INDHOLD'])
+
+    df['TID'] = df['TID'].astype(int)
+    df['INDHOLD'] = df['INDHOLD'].astype(float)
+    df = df.drop(columns=['ALDER'])
+    df = df.rename(columns={'INDHOLD': varname, 'OMRÅDE': 'municipality', 'TID': 'year'})
+
+    # e. sorts
+    df = df.sort_values(by=['municipality'])
+    
+    return df
+
+# Create a function to load STRAFNA7.
+def load_STRAFNA7(OMRÅDE, OVERTRÆD, varname):
+
+    params = {
+        'table': 'STRAFNA7',
+        'format': 'BULK', # semicolon separated file
+        'lang': 'en',
+        'variables': [
+            {'code': 'OMRÅDE', 'values': [OMRÅDE]},
+            {'code': 'OVERTRÆD', 'values': [OVERTRÆD]},
+            {'code': 'Tid', 'values': ['*']},
+            ]
+    }    
+
+    # b. download
+    df = DstApi('STRAFNA7').get_data(params=params)
+
+    # c. set types and rename
+    df['INDHOLD'] = pd.to_numeric(df['INDHOLD'], errors='coerce')
+    df = df.dropna(subset=['INDHOLD'])
+
+    df['TID'] = df['TID'].astype(int)
+    df['INDHOLD'] = df['INDHOLD'].astype(float)
+    df = df.drop(columns=['OVERTRÆD'])
+    df = df.rename(columns={'INDHOLD': varname, 'OMRÅDE': 'municipality', 'TID': 'year'})
+
+    # e. sorts
+    df = df.sort_values(by=['municipality'])
+    
+    return df
+
+# Create a function to load BEFOLK3.
+def load_BEFOLK3(OMRÅDE, KØN, ALDER, varname):
+
+    params = {
+        'table': 'BEFOLK3',
+        'format': 'BULK', # semicolon separated file
+        'lang': 'en',
+        'variables': [
+            {'code': 'OMRÅDE', 'values': [OMRÅDE]},
+            {'code': 'KØN', 'values': [KØN]},
+            {'code': 'ALDER', 'values': [ALDER]},
+            {'code': 'Tid', 'values': ['*']},
+            ]
+    }    
+
+    # b. download
+    df = DstApi('BEFOLK3').get_data(params=params)
+
+    # c. set types and rename
+    df['TID'] = df['TID'].astype(int)
+    df['INDHOLD'] = df['INDHOLD'].astype(float)
+    df = df.drop(columns=['KØN', 'ALDER'])
+    df = df.rename(columns={'INDHOLD': varname, 'OMRÅDE': 'municipality', 'TID': 'year'})
+
+    # e. sorts
+    df = df.sort_values(by=['municipality'])
+    
+    return df
